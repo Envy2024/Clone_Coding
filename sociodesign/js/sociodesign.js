dd85fbe2d21297bb54f4c752ed328856e753d1f4
envy2024 <sohojm3636@naver.com>
2020-08-23 19:18:17 +0900

    4th Commit : 슬라이드 자동 넘김 추가(index.html)

--- sociodesign/js/sociodesign.js
+++ sociodesign/js/sociodesign.js
@@ -1,25 +1,51 @@
 // Main Page Slide
 var control = document.querySelectorAll('.visual .control ul li a'),
   slideSize = control.length,
-  slides = document.querySelectorAll('.visual .slide a');
+  slides = document.querySelectorAll('.visual .slide a'),
+  currentIndex = 0,
+  timer = 4600;
+
+var slideInterval = setInterval(slideIntervalFunc, timer);
+
 for (var i = 0; i < slideSize; i++) {
   control[i].addEventListener('click', function (e) {
     // 다른 요소의 on class 제거
     e.preventDefault();
-    for (var j = 0; j < slideSize; j++) {
-      if (control[j].classList.contains('on')) {
-        control[j].className = '';
-        slides[j].className = '';
-        break;
-      }
-    }
+    clearSlide();
     // 클릭된 요소에 on class 추가
     e.target.className = 'on';
     for (var j = 0; j < slideSize; j++) {
       if (e.target === control[j]) {
         slides[j].className = 'active';
-
+        currentIndex = j;
       }
     }
+    clearInterval(slideInterval);
+    slideInterval = setInterval(slideIntervalFunc, timer);
   });
+}
+
+function slideIntervalFunc() {
+  // setInterval(function () {
+  clearSlide();
+  if (currentIndex < slideSize - 1) { // 4 5
+    currentIndex += 1;
+  } else {
+    currentIndex = 0;
+  }
+  // 클릭된 요소에 on class 추가
+  control[currentIndex].className = 'on';
+  slides[currentIndex].className = 'active';
+
+  // }, 2000);
+}
+
+function clearSlide() {
+  for (var i = 0; i < slideSize; i++) {
+    if (control[i].classList.contains('on')) {
+      control[i].className = '';
+      slides[i].className = '';
+      break;
+    }
+  }
 }
